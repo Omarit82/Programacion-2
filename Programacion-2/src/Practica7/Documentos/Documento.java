3ef20271910aa0202b860e2f8fc5e337fc6d255d
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2023-11-29 22:50:11 -0300

    Add: Guia 7 Filtros

---
+++ Programacion-2/src/Practica7/Documentos/Documento.java
@@ -0,0 +1,51 @@
+package Practica7.Documentos;
+import java.util.ArrayList;
+
+public class Documento {
+    private String titulo;
+    private ArrayList<String> autores;
+    private String contenido;
+    private ArrayList<String> keywords;
+
+    public Documento(String titulo, String contenido) {
+        this.titulo = titulo;
+        this.contenido = contenido;
+        autores = new ArrayList<String>();
+        keywords = new ArrayList<String>();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public void addAutor(String autor){
+        autores.add(autor);
+    }
+    public void addKeyword(String palabra){
+        keywords.add(palabra);
+    }
+
+    public boolean findAutor(String autor){
+        return autores.contains(autor);
+    }
+
+    public boolean findKeyword(String keyword){
+        return keyword.contains(keyword);
+    }
+
+    public String toString(){
+        return "Titulo: "+getTitulo()+" | Contenido: "+ getContenido()+"\n";
+    }
+}
