9e0e735bd7c0c9b2464408354fd6d52392d0c2ae
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2024-01-21 10:42:56 -0300

    Congreso

--- ProgramacionFinalLibre/FinalLibre/src/Clase_1/PRACTICA5/src/Congreso/Congreso.java
+++ ProgramacionFinalLibre/FinalLibre/src/Clase_1/PRACTICA5/src/Congreso/Congreso.java
@@ -33,6 +33,7 @@ public class Congreso {
     }
 
     public ArrayList<Evaluador> puedeEvaluar(Trabajo tt){
+        /* Se chequea que evaluadores pueden evaluar el trabajo pasado por parametro. */
         ArrayList<Evaluador> auxiliar = new ArrayList<>();
         for (Evaluador evaluador : evaluadores) {
             if(tt.puedeEvaluar(evaluador)){
@@ -43,6 +44,7 @@ public class Congreso {
     }
 
     public ArrayList<Trabajo> puedeAsignar(Evaluador ee){
+        /* Se chequea que trabajos puede evaluar el evaluador pasado por parametro */
         ArrayList<Trabajo> auxiliar = new ArrayList<>();
         for (Trabajo trabajo : trabajos) {
             if(trabajo.puedeEvaluar(ee)){
@@ -50,5 +52,20 @@ public class Congreso {
             }
         }
         return auxiliar;
+    }
+
+    public boolean esExperto(Evaluador ee){
+        /* Sera experto si conoce alguno de los temas en el array de temas expertos */
+        int i = 0;
+        boolean experto = false;
+        while (i<temasExpertos.size() && !experto){
+            if(ee.tieneTag(temasExpertos.get(i))){
+                experto = true;
+                System.out.println("El Evaluador: "+ee.getNombre());
+                System.out.println("Es experto en: "+temasExpertos.get(i));
+            }
+            i++;
+        }
+        return experto;
     }
 }
--- ProgramacionFinalLibre/FinalLibre/src/Clase_1/PRACTICA5/src/Congreso/Principal.java
+++ ProgramacionFinalLibre/FinalLibre/src/Clase_1/PRACTICA5/src/Congreso/Principal.java
@@ -5,7 +5,27 @@ public class Principal {
         Evaluador male = new Evaluador("Malena");
         Evaluador omar = new Evaluador("Omar");
 
-        System.out.println(male.getId());
-        System.out.println(omar.getId());
+        Trabajo proy1 = new Trabajo("TP1");
+        Posters post = new Posters("TP2");
+        post.agregarTag("lenguajes de programacion");
+        post.agregarTag("algoritmos");
+        proy1.agregarTag("algoritmos");
+        proy1.agregarTag("Visualizacion");
+
+        omar.agregarTema("visualizacion");
+        omar.agregarTema("algoritmos");
+        male.agregarTema("lenguajes de programacion");
+        Congreso cong = new Congreso();
+
+        cong.addTemaExperto("Visualizacion");
+        cong.addTemaGenerales("algoritmos");
+        cong.agregarEvaluador(omar);
+        cong.agregarEvaluador(male);
+        cong.agregarTrabajo(proy1);
+        cong.agregarTrabajo(post);
+        System.out.println(cong.puedeEvaluar(post));
+        System.out.println(cong.puedeAsignar(omar));
+        System.out.println(cong.esExperto(male));
+        
     }
 }
